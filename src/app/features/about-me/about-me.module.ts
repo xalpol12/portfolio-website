5584cb8cd2536500e61c5e207fce6a5d61c99170
Dawid Stawiszyński <xalpol12@wp.pl>
2024-02-10 00:00:38 +0100

    justify all pages

--- src/app/features/about-me/about-me.module.ts
+++ src/app/features/about-me/about-me.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {AboutMeComponent} from "./about-me.component";
 import {SharedModule} from "../../shared/shared.module";
 import {TechnologiesItemizeComponent} from "./components/technologies-itemize/technologies-itemize.component";
+import {AboutMeDescriptionComponent} from "./pages/about-me-description/about-me-description.component";
 
 
 
 @NgModule({
   declarations: [
     TechnologiesItemizeComponent,
+    AboutMeDescriptionComponent,
     AboutMeComponent,
   ],
   exports: [
---
+++ src/app/features/about-me/pages/about-me-description/about-me-description.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me-description',
+  templateUrl: './about-me-description.component.html',
+  styleUrl: './about-me-description.component.scss'
+})
+export class AboutMeDescriptionComponent {
+}
