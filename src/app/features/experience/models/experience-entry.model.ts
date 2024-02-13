0120bf10be04fa60c52bb14615c668d635d48f2c
Dawid Stawiszyński <xalpol12@wp.pl>
2024-02-13 17:03:29 +0100

    provide sane and socially acceptable descriptions

--- src/app/features/experience/models/experience-entry.model.ts
+++ src/app/features/experience/models/experience-entry.model.ts
@@ -29,27 +29,4 @@ export const experience: ExperienceEntryModel[] = [
       `
     ]
   },
-  {
-    tabTitle: "Placeholder #1",
-    fullTitle: "Java Backend Engineering Intern",
-    companyName: "WSRH",
-    companyLink: "https://pl.wikipedia.org/wiki/WSRH",
-    startDate: "Sep 2023",
-    endDate: "Dec 2023",
-    description: [
-      `
-      WSRH (skrót od Wyższa Szkoła Robienia Hałasu) – polski zespół muzyczny wykonujący hip-hop.
-      Powstał w 2008 roku z inicjatywy poznańskich raperów Shelleriniego i Słonia.
-      `,
-      `
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius maximus justo et luctus.
-      Nulla eget nunc euismod, posuere dolor tempus, gravida justo. Integer at commodo elit.
-      Nunc id tempus metus, vel fringilla ligula. Vestibulum porta erat ut justo maximus tempor.
-      Integer efficitur tempus enim. Proin pretium leo nec elit tempus, nec consectetur risus semper.
-      Phasellus in ligula in lacus ultricies semper. Curabitur ipsum nisi, euismod ac convallis a, tincidunt id urna.
-      Suspendisse potenti. Etiam quis aliquam tellus, in faucibus justo. Vivamus vitae ex id elit tempus fermentum.
-      Duis quis gravida enim.
-      `
-    ]
-  },
 ]
