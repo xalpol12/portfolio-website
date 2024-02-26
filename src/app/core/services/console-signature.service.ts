a8b1cadf0fa7e605019569225b7550df5a461625
xalpol12 <xalpol12@gmail.com>
2024-02-26 20:59:15 +0100

    check check

--- src/app/core/services/console-signature.service.ts
+++ src/app/core/services/console-signature.service.ts
@@ -14,6 +14,7 @@ export class ConsoleSignatureService {
 ██╔╝ ██╗██║  ██║███████╗██║     ╚██████╔╝███████╗██║███████╗
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝      ╚═════╝ ╚══════╝╚═╝╚══════╝
 for more projects check out my github: https://github.com/xalpol12
+    modified version 26.02 used for debugging ci/cd with gh pages
 `,
       'font-family: monospace; color: #A6E3A1; font-size: 12px;'
     );
