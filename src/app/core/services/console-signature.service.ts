78dae0a03879314379241cd53dda0b606ba29385
xalpol12 <xalpol12@gmail.com>
2024-02-26 21:14:17 +0100

    delete useless console log

--- src/app/core/services/console-signature.service.ts
+++ src/app/core/services/console-signature.service.ts
@@ -14,7 +14,6 @@ export class ConsoleSignatureService {
 ██╔╝ ██╗██║  ██║███████╗██║     ╚██████╔╝███████╗██║███████╗
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝      ╚═════╝ ╚══════╝╚═╝╚══════╝
 for more projects check out my github: https://github.com/xalpol12
-    modified version used for debugging ci/cd with gh pages, try 2
 `,
       'font-family: monospace; color: #A6E3A1; font-size: 12px;'
     );
