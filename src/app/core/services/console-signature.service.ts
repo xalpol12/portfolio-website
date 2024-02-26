97a3b03936dc22689032d015fb4100941d26fe35
xalpol12 <xalpol12@gmail.com>
2024-02-26 21:11:18 +0100

    fix url

--- src/app/core/services/console-signature.service.ts
+++ src/app/core/services/console-signature.service.ts
@@ -14,7 +14,7 @@ export class ConsoleSignatureService {
 ██╔╝ ██╗██║  ██║███████╗██║     ╚██████╔╝███████╗██║███████╗
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝      ╚═════╝ ╚══════╝╚═╝╚══════╝
 for more projects check out my github: https://github.com/xalpol12
-    modified version 26.02 used for debugging ci/cd with gh pages
+    modified version used for debugging ci/cd with gh pages, try 2
 `,
       'font-family: monospace; color: #A6E3A1; font-size: 12px;'
     );
