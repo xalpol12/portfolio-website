a77acc540d002a50d37f6d9ecd46e8c5fee3f186
xalpol12 <xalpol12@gmail.com>
2024-02-29 09:24:58 +0100

    modify project descriptions, add on hover title color change

--- src/app/features/projects/models/project-card-entry.ts
+++ src/app/features/projects/models/project-card-entry.ts
@@ -13,9 +13,10 @@ export const projects: ProjectCardEntry[] = [
     title: "Kuka AR (Backend)",
     techStack: ["C#", "Unity", "Java + Spring"],
     frontDescription: `IoT system for real-time tracking of industrial robot's coordinate system
-    using augmented reality. Android app and back-end server created for engineering thesis.`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    using augmented reality. `,
+    backDescription: `Spring Web server containing REST module for managing static data, websocket
+    module for real-time data acquisition and transmission. Client Android app uses AR Foundation framework
+    for image recognition and data visualization in a 3D scene.`,
     ghLink: "https://github.com/xalpol12/kuka-ar",
     ytLink: "https://youtu.be/biTEiVqRQYo",
   },
@@ -23,38 +24,36 @@ export const projects: ProjectCardEntry[] = [
     title: "Messenger Bot (Fullstack)",
     techStack: ["Java + Spring", "Typescript + Angular", "Docker"],
     frontDescription: "Soon to be completed.",
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    backDescription: `Completion level: 80% - implementing frontend`,
     ghLink: "https://github.com/xalpol12/messenger-bot",
     ytLink: "https://www.youtube.com/watch?v=qU8UfYdKHvs",
-    outsideLink: "https://www.google.pl/",
+    // outsideLink: "https://www.google.pl/",
   },
   {
     title: "Where to Eat (Fullstack)",
     techStack: ["Java + Spring", "Android app"],
-    frontDescription: `Android app that recommends user a list of restaurants in specified proximity,
-    based on phone's GPS sensor and Google Places API.`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    frontDescription: `Android app that recommends user a list of restaurants in specified proximity, based on their location
+    based on and Google Places API.`,
+    backDescription: `Spring backend server acting as a wrapper for Google Places API.
+    DI library (Hilt + Dagger) with Room database for an Android app. Mobile app uses measurements
+    from phone's GPS sensor.`,
     ghLink: "https://github.com/xalpol12/where-to-eat-android-app",
   },
   {
-    title: "Portfolio Page (Frontend)",
+    title: "Portfolio Website (Frontend)",
     techStack: ["Typescript + Angular"],
-    frontDescription: `Portfolio SPA that aims to present information and list
-    the best projects in a coherent way. Leveraging bootstrap library and cattpuccin color scheme with retro
-    terminal aesthetic to visualize my areas of interest.`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    frontDescription: `SPA that aims to present information and list
+    the best projects in a coherent way. `,
+    backDescription: `Leveraging ng-bootstrap library and catppuccin color scheme with retro
+    terminal aesthetic to visualize my areas of interest. Modular project structure that promotes
+    expandability.`,
     ghLink: "https://github.com/xalpol12/portfolio-website",
   },
   {
     title: "Kinect Robot Controller",
     techStack: ["Python", "ROS2", "Linux"],
-    frontDescription: `ROS2 package for controlling a robot using gestures.
-    Implemented with image processing algorithms using OpenCV and Kinect v1 depth sensor`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    frontDescription: `ROS2 package for controlling a robot using gestures.`,
+    backDescription: `Implemented with image processing algorithms using OpenCV and Kinect v1 depth sensor.`,
     ghLink: "https://github.com/xalpol12/kinect-robot-controller",
     ytLink: "https://www.youtube.com/watch?v=vXd0x3p9swo"
   },
@@ -62,20 +61,18 @@ export const projects: ProjectCardEntry[] = [
     title: "JSON to XML Adapter",
     techStack: ["Java"],
     frontDescription: `CLI application for converting specific JSON files to
-    established XML hierarchy. Project involved creating a POJO representation
-    of XML file structure, document deserialization, DOM manipulation and serialization.`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
+    established XML hierarchy. `,
+    backDescription: `Project involved creating a POJO representation of XML
+    file structure, document deserialization, DOM manipulation and serialization.`,
     ghLink: "https://github.com/xalpol12/festo-xml-json-adapter",
   },
   {
     title: "Candy Recognizer",
     techStack: ["Python"],
     frontDescription: `App that recognizes and returns a count of m&m's candies
-    of each color in a provided photo. Implemented with image processing
+    of each color in a provided photo. `,
+    backDescription: `Implemented with image processing
     algorithms using OpenCV library.`,
-    backDescription: `This is a back description that I haven't provided yet.
-    Here will be some other details in case front description is too long.`,
     ghLink: "https://github.com/xalpol12/candy-recognizer",
   },
 ]
